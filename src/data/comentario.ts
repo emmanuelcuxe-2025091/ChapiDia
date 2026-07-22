a1d3d8b13c4150e67a3b3e374bf8b80ac99ec8d4
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-07-22 11:14:10 -0600

    feat: agrega archivo JSON y arreglo de Comentario

---
+++ src/data/comentario.ts
@@ -0,0 +1,12 @@
+import { Comentario } from '../models/comentario';
+
+export const comentarios: Comentario[] = [
+    {
+        idComentario: 1,
+        texto: "Espectacular fotografía.",
+        fecha: new Date("2026-07-21"),
+        fechaModificacion: new Date("2026-07-22"),
+        idUsuario: 1,
+        idNoticia: 1
+    }
+];
