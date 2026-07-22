2dcb44aaac64c52a50eed0e13ca718b77a8405b6
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-07-22 10:28:06 -0600

    feat: agrega archivo JSON y arreglo de Categoria

---
+++ src/data/categoria.ts
@@ -0,0 +1,9 @@
+import { Categoria } from '../models/categoria';
+
+export const categorias: Categoria[] = [
+    {
+        idCategoria: 1,
+        nombreCategoria: "Naturaleza",
+        descripcion: "Una fotografía de paisajes y fauna."
+    }
+];
