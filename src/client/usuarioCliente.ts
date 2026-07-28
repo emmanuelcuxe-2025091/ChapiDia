fb96b4fa5749bf997167229b7bfd3583cb4f1430
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-07-28 00:14:39 -0600

    feat: crea cliente para la entidad de Usuario

---
+++ src/client/usuarioCliente.ts
@@ -0,0 +1,53 @@
+import { Usuario } from "../models/usuario";
+
+export class UsuarioClient {
+    private URL = "http://localhost:3000/usuarios";
+
+    public async obtenerUsuarios(): Promise<Usuario[]> {
+        const respuesta = await fetch(this.URL);
+
+        return await respuesta.json();
+    }
+
+
+    public async obtenerUsuarioPorId(id: number): Promise<Usuario> {
+        const respuesta = await fetch(`${this.URL}/${id}`);
+
+        if (!respuesta.ok) {
+            throw new Error("Usuario no encontrado");
+        }
+
+        return await respuesta.json();
+    }
+
+
+    public async crearUsuario(usuario: Usuario): Promise<any> {
+        const respuesta = await fetch(this.URL, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(usuario)
+        });
+
+        return await respuesta.json();
+    }
+
+
+    public async actualizarUsuario(id: number, usuario: Usuario): Promise<any> {
+        const respuesta = await fetch(`${this.URL}/${id}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(usuario)
+        });
+
+        return await respuesta.json();
+    }
+    
+
+    public async eliminarUsuario(id: number): Promise<any> {
+        const respuesta = await fetch(`${this.URL}/${id}`, {
+            method: "DELETE"
+        });
+
+        return await respuesta.json();
+    }
+}
