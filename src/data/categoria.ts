49c49329acebf66e2730ef9eb85bb5253af859ad
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-07-27 19:29:45 -0600

    refactor: elimina archivos antiguos

--- src/data/categoria.ts
+++
@@ -1,9 +0,0 @@
-import { Categoria } from '../models/categoria';
-
-export const categorias: Categoria[] = [
-    {
-        idCategoria: 1,
-        nombreCategoria: "Naturaleza",
-        descripcion: "Una fotografía de paisajes y fauna."
-    }
-];
