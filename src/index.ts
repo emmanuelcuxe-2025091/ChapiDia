1597400e00ce9a575c9c9760d90f0dd9978d0332
emmanuelcuxe-2025091 <ecuxe-2025091@kinal.edu.gt>
2026-07-28 11:02:58 -0600

    feat: llama método para iniciar el servidor

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import "./api/server";
+import { iniciar } from "./api/server";
 import { menu } from "./menu/menu";
 
 async function main() {
-
+    
+    await iniciar();
     await menu();
     
 }
+
+main();
